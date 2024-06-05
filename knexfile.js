fe40ad2d4f916e9373ab71f331c3669282b753fe
Graciela MacBook <moragraa@gmail.com>
2024-06-04 22:51:05 -0600

    added knexfile, migrations, created tables, changed auth-collection to connect it with DB

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,6 @@ module.exports = {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    charset: "utf8",
+    password: process.env.DB_PASSWORD
   },
 };
